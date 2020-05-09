bc26b50959742b29a9eb7563ca2516d929ce78ba
LucasArvid <lucas.arvidsson@hotmail.com>
2020-05-09 23:04:59 +0200

    removed all comments, logs and traces

--- app/src/main/java/com/example/imageclassification/MainActivity.java
+++ app/src/main/java/com/example/imageclassification/MainActivity.java
@@ -1,19 +1,10 @@
 package com.example.imageclassification;
 
-import android.app.Activity;
 import android.content.res.AssetFileDescriptor;
-import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.RectF;
-import android.media.Image;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.Interpreter;
@@ -25,41 +16,26 @@ import org.tensorflow.lite.support.image.ImageProcessor;
 import org.tensorflow.lite.support.image.TensorImage;
 import org.tensorflow.lite.support.image.ops.ResizeOp;
 import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp;
-import org.tensorflow.lite.support.image.ops.Rot90Op;
 import org.tensorflow.lite.support.label.TensorLabel;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 
-import android.os.SystemClock;
-import android.os.Trace;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.stream.Collectors;
 
 
 public class MainActivity extends AppCompatActivity {
     private static final int EPOCHS = 100;
-
-    static final int IMG_SIZE_X = 224;
-    static final int IMG_SIZE_Y = 224;
 
     private static final String MODEL_PATH = "mobilenet_v1_1.0_224.tflite";
     private static final String LABELS_PATH = "mobilenet_v1_1.0_224.txt";
@@ -69,8 +45,6 @@ public class MainActivity extends AppCompatActivity {
     private TensorProcessor probabilityProcessor;
     Map<String, Float> floatMap;
 
-    private  int imageSizeX;
-    private  int imageSizeY;
     private Bitmap bitmap;
 
     private List<Recognition> recognitions;
@@ -90,18 +64,11 @@ public class MainActivity extends AppCompatActivity {
         setupInterpreter();
         loadLabels();
 
-        Trace.beginSection("runInference");
-        long startTimeForReference = SystemClock.uptimeMillis();
         for (int i = 1; i <= EPOCHS; i++) {
             loadBitmap(String.format("dataset/image_00%d.jpg", i));
             recognitions = recognizeImage();
             //runInferenceOnImage();
         }
-        long endTimeForReference = SystemClock.uptimeMillis();
-        Trace.endSection();
-        System.out.println("Timecost to run model inference: " + (endTimeForReference - startTimeForReference));
-        System.out.println(recognitions.get(0).getTitle());
-        System.out.println(recognitions.get(0).getConfidence());
 
         closeTflite();
         setupLabels();
@@ -115,10 +82,9 @@ public class MainActivity extends AppCompatActivity {
             bitmap = BitmapFactory.decodeStream(this.getAssets().open(filepath));
         } catch (IOException e) {
             e.printStackTrace();
-            Log.e("bitmapError",  "failed big time boi", e);
         }
         System.out.println(filepath);
-        inputImageBuffer = new TensorImage(tflite.getInputTensor(0).dataType()); // or Data.Type.FLOAT32
+        inputImageBuffer = new TensorImage(tflite.getInputTensor(0).dataType());
         inputImageBuffer.load(bitmap);
 
         int cropSize = Math.min(bitmap.getWidth(), bitmap.getHeight());
@@ -136,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
             associatedAxisLabels = FileUtil.loadLabels(this, LABELS_PATH);
             labels = FileUtil.loadLabels(this, LABELS_PATH);
         } catch (IOException e) {
-            Log.e("tfliteError", "error reading labels", e);
+
         }
         outputProbabilityBuffer = TensorBuffer.createFixedSize(new int[]{1, 1001}, DataType.FLOAT32);
 
@@ -158,7 +124,6 @@ public class MainActivity extends AppCompatActivity {
         try {
             tflite = new Interpreter(loadModelFile(), tfliteOptions);
         } catch (IOException e) {
-            Log.e("tfliteError","error loding model", e);
         }
     }
 
@@ -179,15 +144,8 @@ public class MainActivity extends AppCompatActivity {
     private TensorOperator getPostprocessNormalizeOp() {
         return new NormalizeOp(0.0f, 1.0f);
     }
-    /** Function not using labels, clean up before release*/
-    /*
-    private void runInferenceOnImage() {
-        tflite.run(inputImageBuffer.getBuffer(), outputProbabilityBuffer.getBuffer().rewind());
-    }
-    */
-    /** Gets the top-k results. */
+
     private static List<Recognition> getTopKProbability(Map<String, Float> labelProb) {
-        // Find the best classifications.
         PriorityQueue<Recognition> pq =
                 new PriorityQueue<>(
                         3,
@@ -211,27 +169,14 @@ public class MainActivity extends AppCompatActivity {
         return recognitions;
     }
 
-    /** Runs inference and returns the classification results. */
     public List<Recognition> recognizeImage() {
-        // Logs this method so that it can be analyzed with systrace.
-        Trace.beginSection("recognizeImage");
-        /** Clean up logs before release*/
-        // Runs the inference call.
-        Trace.beginSection("runInference");
-        long startTimeForReference = SystemClock.uptimeMillis();
         tflite.run(inputImageBuffer.getBuffer(), outputProbabilityBuffer.getBuffer().rewind());
-        long endTimeForReference = SystemClock.uptimeMillis();
-        Trace.endSection();
-        System.out.println("Timecost to run model inference: " + (endTimeForReference - startTimeForReference));
-
         probabilityProcessor = new TensorProcessor.Builder().add(getPostprocessNormalizeOp()).build();
-        // Gets the map of label and probability.
+
         Map<String, Float> labeledProbability =
                 new TensorLabel(labels, probabilityProcessor.process(outputProbabilityBuffer))
                         .getMapWithFloatValue();
-        Trace.endSection();
 
-        // Gets top-k results.
         return getTopKProbability(labeledProbability);
     }
     private void closeTflite() {
@@ -241,27 +186,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
-
-
-
-
 }
--- app/src/main/java/com/example/imageclassification/Recognition.java
+++ app/src/main/java/com/example/imageclassification/Recognition.java
@@ -2,23 +2,14 @@ package com.example.imageclassification;
 
 import android.graphics.RectF;
 
-/** An immutable result returned by a Classifier describing what was recognized. */
 public class Recognition {
-    /**
-     * A unique identifier for what has been recognized. Specific to the class, not the instance of
-     * the object.
-     */
+
     private final String id;
 
-    /** Display name for the recognition. */
     private final String title;
 
-    /**
-     * A sortable score for how good the recognition is relative to others. Higher should be better.
-     */
     private final Float confidence;
 
-    /** Optional location within the source image for the location of the recognized object. */
     private RectF location;
 
     public Recognition(
